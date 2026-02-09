1e9facda1321ccc802ada857a6de95a86212d4a0
Ken Murase <lax.potcx1562@gmail.com>
2026-02-09 18:07:57 +0900

    コンパイルエラー修正

--- core/src/commonMain/kotlin/terakoyalabo/core/domain/quantity/ChronosExtensions.kt
+++ core/src/commonMain/kotlin/terakoyalabo/core/domain/quantity/ChronosExtensions.kt
@@ -6,16 +6,16 @@ import terakoyalabo.core.domain.quantity.model.ChronosMs
 
 // ミリ秒：最小単位そのまま
 val Int.ms: ChronosMs get() = ChronosMs.of(this.sd)
-val Double.ms: ChronosMs get() = ChronosMs.of(this.sVal)
+val Double.ms: ChronosMs get() = ChronosMs.of(this.sd)
 
 // 秒：量に KILO (1000) を掛けてミリ秒へ
 val Int.s: ChronosMs get() = ChronosMs.of(this.sd * ScalarD.KILO)
-val Double.s: ChronosMs get() = ChronosMs.of(this.sVal * ScalarD.KILO)
+val Double.s: ChronosMs get() = ChronosMs.of(this.sd * ScalarD.KILO)
 
 // 分：秒に SEXA (60) を掛けてミリ秒へ
 val Int.min: ChronosMs get() = ChronosMs.of(this.sd * ScalarD.KILO * ScalarD.SEXA)
-val Double.min: ChronosMs get() = ChronosMs.of(this.sVal * ScalarD.KILO * ScalarD.SEXA)
+val Double.min: ChronosMs get() = ChronosMs.of(this.sd * ScalarD.KILO * ScalarD.SEXA)
 
 // 時：分に SEXA (60) を掛けてミリ秒へ
 val Int.hour: ChronosMs get() = ChronosMs.of(this.sd * ScalarD.KILO * ScalarD.SEXA * ScalarD.SEXA)
-val Double.hour: ChronosMs get() = ChronosMs.of(this.sVal * ScalarD.KILO * ScalarD.SEXA * ScalarD.SEXA)
+val Double.hour: ChronosMs get() = ChronosMs.of(this.sd * ScalarD.KILO * ScalarD.SEXA * ScalarD.SEXA)
